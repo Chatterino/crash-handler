b06ef0d6b6b611ab36fbec986c16a7bc9e18a63c
Nerixyz <nerixdev@outlook.de>
2024-10-14 18:03:30 +0200

    fix: use `<magic_enum/magic_enum.hpp>` to include

--- src/WinSupport.cpp
+++ src/WinSupport.cpp
@@ -6,7 +6,7 @@
 #define MAGIC_ENUM_USING_ALIAS_STRING_VIEW \
     using string_view = std::wstring_view;
 #define MAGIC_ENUM_USING_ALIAS_STRING using string = std::wstring;
-#include <magic_enum.hpp>
+#include <magic_enum/magic_enum.hpp>
 
 using namespace std::string_literals;
 
